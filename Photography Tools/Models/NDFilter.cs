28698919203c021d48d90519c619735127a2b676
Pb2k19 <xxmordorxx@wp.pl>
2024-02-23 15:16:27 +0100

    Addition of "name" property in NDFilter model

--- Photography Tools/Models/NDFilter.cs
+++ Photography Tools/Models/NDFilter.cs
@@ -2,6 +2,7 @@
 
 public class NDFilter
 {
+    public required string Name { get; set; }
     public int FStopReduction { get; set; }
     public double OpticalDensity { get; set; }
     public uint Factor { get; set; }
