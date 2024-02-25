15552c9ef433d1487c873b1baed4a1ec162376e1
Pb2k19 <xxmordorxx@wp.pl>
2024-02-25 16:33:53 +0100

    NdFilterCalcPage improvements

--- Photography Tools/Pages/NdFilterCalcPage.xaml.cs
+++ Photography Tools/Pages/NdFilterCalcPage.xaml.cs
@@ -7,4 +7,50 @@ public partial class NdFilterCalcPage : ContentPage
 		InitializeComponent();
 		BindingContext = ndFilterCalcViewModel;
 	}
+
+#if ANDROID
+    private const int Offset = 15;
+    private double lastWidth = 0;
+    private bool isOneColumn = true;
+
+    protected override void OnSizeAllocated(double width, double height)
+    {
+        base.OnSizeAllocated(width, height);
+        if (lastWidth != width)
+        {
+            lastWidth = width;
+
+            if (!isOneColumn && width < height) //vertical
+            {
+                isOneColumn = true;
+                FiltersCollectionView.ItemSizingStrategy = ItemSizingStrategy.MeasureFirstItem;
+                GridItemsLayout.Span = 1;
+            }
+            else if (isOneColumn && width > height) //horizontal
+            {
+                isOneColumn = false;
+                FiltersCollectionView.ItemSizingStrategy = ItemSizingStrategy.MeasureAllItems;
+                GridItemsLayout.Span = 2;
+            }
+        }
+    }
+#elif WINDOWS
+    private const int MinTwoLineWidth = 650;
+    private bool isOneColumn = false;
+    
+    protected override void OnSizeAllocated(double width, double height)
+    {
+        base.OnSizeAllocated(width, height);
+        if (isOneColumn && width > MinTwoLineWidth)
+        {
+            isOneColumn = false;
+            GridItemsLayout.Span = 2;
+        }
+        else if (!isOneColumn && width < MinTwoLineWidth)
+        {
+            isOneColumn = true;
+            GridItemsLayout.Span = 1;
+        }
+    }
+#endif
 }
