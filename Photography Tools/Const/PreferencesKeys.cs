b375788532d435992a97164590e54c8814f04087
Pb2k19 <xxmordorxx@wp.pl>
2024-03-05 12:47:36 +0100

    PreferencesKeys implementation

---
+++ Photography Tools/Const/PreferencesKeys.cs
@@ -0,0 +1,9 @@
+﻿namespace Photography_Tools.Const;
+
+public static class PreferencesKeys
+{
+    public const string
+        AstroTimeCalcViewModelPreferencesKey = "AstroTimeCalcViewModelPreferences",
+        DofCalcViewModelPreferencesKey = "DofCalcViewModelPreferences",
+        NDFilterCalcViewModelPreferencesKey = "NDFilterCalcViewModelPreferences";
+}
