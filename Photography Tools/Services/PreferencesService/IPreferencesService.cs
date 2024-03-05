e6b5d7bc9f277cee57c900d064a7613a75058ced
Pb2k19 <xxmordorxx@wp.pl>
2024-03-05 11:13:22 +0100

    PreferencesService implementation

---
+++ Photography Tools/Services/PreferencesService/IPreferencesService.cs
@@ -0,0 +1,7 @@
+﻿namespace Photography_Tools.Services.PreferencesService;
+
+public interface IPreferencesService
+{
+    T? GetPreference<T>(string key, T? defaultValue = default);
+    bool SetPreference<T>(string key, T value);
+}
---
+++ Photography Tools/Services/PreferencesService/PreferencesService.cs
@@ -0,0 +1,21 @@
+﻿namespace Photography_Tools.Services.PreferencesService;
+
+public class PreferencesService : IPreferencesService
+{
+    public T? GetPreference<T>(string key, T? defaultValue = default)
+    {
+        return Preferences.Default.Get(key, defaultValue);
+    }
+
+    public bool SetPreference<T>(string key, T value)
+    {
+        if (value is null)
+            return false;
+
+        if (value.Equals(GetPreference<T>(key)))
+            return true;
+
+        Preferences.Default.Set(key, value);
+        return true;
+    }
+}
