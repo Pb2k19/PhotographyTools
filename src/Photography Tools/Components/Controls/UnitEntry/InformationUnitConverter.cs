46889f6e8f21e710c2b409465627d7af2d9ebd38
Pb2k19 <xxmordorxx@wp.pl>
2024-03-28 18:36:21 +0100

    InformationUnitConverter implementation

---
+++ src/Photography Tools/Components/Controls/UnitEntry/InformationUnitConverter.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Immutable;
+
+namespace Photography_Tools.Components.Controls.UnitEntry;
+
+public class InformationUnitConverter : IUnitConverter
+{
+    public string BaseUnitName => InformationUnitsConst.Megabyte;
+
+    public ImmutableArray<string> Units => InformationUnitsConst.Units;
+
+    public double ConvertBaseToSelectedUnit(double inputValue, string outputUnit)
+    {
+        if (inputValue == 0)
+            return 0;
+
+        checked
+        {
+            return outputUnit switch
+            {
+                InformationUnitsConst.Byte => inputValue * 1_000_000,
+                InformationUnitsConst.Kilobyte => inputValue * 1_000,
+                InformationUnitsConst.Megabyte => inputValue,
+                InformationUnitsConst.Gigabyte => inputValue / 1_000,
+                InformationUnitsConst.Terabyte => inputValue / 1_000_000,
+                _ => throw new ArgumentException("Unsupported unit", nameof(outputUnit))
+            };
+        }
+    }
+
+    public double ConvertToBaseUnit(double inputValue, string inputUnit)
+    {
+        if (inputValue == 0)
+            return 0;
+
+        checked
+        {
+            return inputUnit switch
+            {
+                InformationUnitsConst.Byte => inputValue / 1_000_000,
+                InformationUnitsConst.Kilobyte => inputValue / 1_000,
+                InformationUnitsConst.Megabyte => inputValue,
+                InformationUnitsConst.Gigabyte => inputValue * 1_000,
+                InformationUnitsConst.Terabyte => inputValue * 1_000_000,
+                _ => throw new ArgumentException("Unsupported unit", nameof(inputUnit))
+            };
+        }
+    }
+}
---
+++ src/PhotographyToolsTests/ComponentsTests/UnitEntryTests/InformationUnitConverterTests.cs
@@ -0,0 +1,167 @@
+﻿using Photography_Tools.Components.Controls.UnitEntry;
+using Photography_Tools.Const;
+
+namespace PhotographyToolsTests.ComponentsTests.UnitEntryTests;
+
+public class InformationUnitConverterTests
+{
+    [Theory]
+    [InlineData(0, 0)]
+    [InlineData(10, 0.00001)]
+    [InlineData(-15, -0.000015)]
+    [InlineData(23.706, 0.000023706)]
+    public void ConvertToBaseUnit_ShouldReturnConvertedValueFromBToMB(double inputValue, double expected)
+    {
+        InformationUnitConverter converter = new();
+
+        double actual = converter.ConvertToBaseUnit(inputValue, InformationUnitsConst.Byte);
+
+        Assert.Equal(expected, actual, 15);
+    }
+
+    [Theory]
+    [InlineData(0, 0)]
+    [InlineData(10, 0.01)]
+    [InlineData(-150, -0.15)]
+    [InlineData(23.706, 0.023706)]
+    public void ConvertToBaseUnit_ShouldReturnConvertedValueFromkBToMB(double inputValue, double expected)
+    {
+        InformationUnitConverter converter = new();
+
+        double actual = converter.ConvertToBaseUnit(inputValue, InformationUnitsConst.Kilobyte);
+
+        Assert.Equal(expected, actual, 15);
+    }
+
+    [Theory]
+    [InlineData(0, 0)]
+    [InlineData(10, 10)]
+    [InlineData(-150, -150)]
+    [InlineData(23.706, 23.706)]
+    public void ConvertToBaseUnit_ShouldReturnConvertedValueFromMBToMB(double inputValue, double expected)
+    {
+        InformationUnitConverter converter = new();
+
+        double actual = converter.ConvertToBaseUnit(inputValue, InformationUnitsConst.Megabyte);
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData(0, 0)]
+    [InlineData(10.0005, 10_000.5)]
+    [InlineData(-150, -150_000)]
+    [InlineData(23.706, 23706)]
+    public void ConvertToBaseUnit_ShouldReturnConvertedValueFromGBToMB(double inputValue, double expected)
+    {
+        InformationUnitConverter converter = new();
+
+        double actual = converter.ConvertToBaseUnit(inputValue, InformationUnitsConst.Gigabyte);
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData(0, 0)]
+    [InlineData(10.0000005, 10000000.5)]
+    [InlineData(-150, -150000000)]
+    [InlineData(23.706, 23706000)]
+    public void ConvertToBaseUnit_ShouldReturnConvertedValueFromTBToMB(double inputValue, double expected)
+    {
+        InformationUnitConverter converter = new();
+
+        double actual = converter.ConvertToBaseUnit(inputValue, InformationUnitsConst.Terabyte);
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData(0, 0)]
+    [InlineData(10, 10000000)]
+    [InlineData(-150, -150000000)]
+    [InlineData(23.706, 23706000)]
+    public void ConvertBaseToSelectedUnit_ShouldReturnConvertedValueFromMBToB(double inputValue, double expected)
+    {
+        InformationUnitConverter converter = new();
+
+        double actual = converter.ConvertBaseToSelectedUnit(inputValue, InformationUnitsConst.Byte);
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData(0, 0)]
+    [InlineData(10, 10000)]
+    [InlineData(-150, -150000)]
+    [InlineData(23.706, 23706)]
+    public void ConvertBaseToSelectedUnit_ShouldReturnConvertedValueFromMBTokB(double inputValue, double expected)
+    {
+        InformationUnitConverter converter = new();
+
+        double actual = converter.ConvertBaseToSelectedUnit(inputValue, InformationUnitsConst.Kilobyte);
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData(0, 0)]
+    [InlineData(10, 10)]
+    [InlineData(-150, -150)]
+    [InlineData(23.706, 23.706)]
+    public void ConvertBaseToSelectedUnit_ShouldReturnConvertedValueFromMBToMB(double inputValue, double expected)
+    {
+        InformationUnitConverter converter = new();
+
+        double actual = converter.ConvertBaseToSelectedUnit(inputValue, InformationUnitsConst.Megabyte);
+
+        Assert.Equal(expected, actual, 15);
+    }
+
+    [Theory]
+    [InlineData(0, 0)]
+    [InlineData(10, 0.01)]
+    [InlineData(-150, -0.15)]
+    [InlineData(23.706, 0.023706)]
+    public void ConvertBaseToSelectedUnit_ShouldReturnConvertedValueFromMBToGB(double inputValue, double expected)
+    {
+        InformationUnitConverter converter = new();
+
+        double actual = converter.ConvertBaseToSelectedUnit(inputValue, InformationUnitsConst.Gigabyte);
+
+        Assert.Equal(expected, actual, 15);
+    }
+
+    [Theory]
+    [InlineData(0, 0)]
+    [InlineData(10, 0.00001)]
+    [InlineData(-150, -0.00015)]
+    [InlineData(23.706, 0.000023706)]
+    public void ConvertBaseToSelectedUnit_ShouldReturnConvertedValueFromMBToTB(double inputValue, double expected)
+    {
+        InformationUnitConverter converter = new();
+
+        double actual = converter.ConvertBaseToSelectedUnit(inputValue, InformationUnitsConst.Terabyte);
+
+        Assert.Equal(expected, actual, 15);
+    }
+
+    [Fact]
+    public void ConvertBaseToSelectedUnit_ShouldThrowArgumentException()
+    {
+        LengthUnitConverter converter = new();
+
+        ArgumentException ex = Assert.Throws<ArgumentException>(() => converter.ConvertBaseToSelectedUnit(20, "Unsupported"));
+
+        Assert.Equal("Unsupported unit (Parameter 'outputUnit')", ex.Message);
+    }
+
+    [Fact]
+    public void ConvertToBaseUnit_ShouldThrowArgumentException()
+    {
+        LengthUnitConverter converter = new();
+
+        ArgumentException ex = Assert.Throws<ArgumentException>(() => converter.ConvertToBaseUnit(20, "Unsupported"));
+
+        Assert.Equal("Unsupported unit (Parameter 'inputUnit')", ex.Message);
+    }
+}
