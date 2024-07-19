a1458d216afac82ca1516fe2ef517a72c3c3d45f
Patryk Bartołd <xxmordorxx@wp.pl>
2024-07-19 22:47:12 +0200

    GetValueOrNull implementation

---
+++ src/Photography Tools/Helpers/CollectionHelper.cs
@@ -0,0 +1,11 @@
+﻿namespace Photography_Tools.Helpers;
+
+public static class CollectionHelper
+{
+    public static TValue? GetValueOrNull<TKey, TValue>(this IReadOnlyDictionary<TKey, TValue> dictionary, TKey key) where TValue : struct
+    {
+        ArgumentNullException.ThrowIfNull(dictionary);
+
+        return dictionary.TryGetValue(key, out TValue value) ? value : null;
+    }
+}
---
+++ src/PhotographyToolsTests/HelpersTests/CollectionHelperTests.cs
@@ -0,0 +1,43 @@
+﻿using Photography_Tools.Helpers;
+
+namespace PhotographyToolsTests.HelpersTests
+{
+    public class CollectionHelperTests
+    {
+        private static readonly Dictionary<string, DateTime> DateTimeTestDictionary = new()
+        {
+            { "Test1", new(1900, 5, 6, 7, 18, 14) },
+            { "Test2", new(2134, 5, 6, 7, 18, 41) },
+            { "Test3", new(2500, 5, 6, 7, 18, 7) },
+        };
+
+        [Fact]
+        public void GetValueOrNull_ShouldReturnValue()
+        {
+            DateTime expected = new(2134, 5, 6, 7, 18, 41);
+
+            DateTime? actual = DateTimeTestDictionary.GetValueOrNull("Test2");
+
+            Assert.NotNull(actual);
+            Assert.Equal(expected, actual.Value);
+        }
+
+        [Fact]
+        public void GetValueOrNull_ShouldReturnNull()
+        {
+            DateTime? actual = DateTimeTestDictionary.GetValueOrNull("T");
+
+            Assert.Null(actual);
+        }
+
+        [Fact]
+        public void GetValueOrNull_ShouldArgumentNullException()
+        {
+            Dictionary<string, int>? nullDictionary = null;
+
+#pragma warning disable CS8604 // Possible null reference argument.
+            Assert.Throws<ArgumentNullException>(() => nullDictionary.GetValueOrNull("Test2"));
+#pragma warning restore CS8604 // Possible null reference argument.
+        }
+    }
+}
