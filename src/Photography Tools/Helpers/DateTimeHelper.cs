56cdf5dda73cafbef47193d079833cae21506b40
Patryk Bartołd <xxmordorxx@wp.pl>
2024-08-05 14:06:46 +0200

    DateTimeHelper improvements

--- src/Photography Tools/Helpers/DateTimeHelper.cs
+++ src/Photography Tools/Helpers/DateTimeHelper.cs
@@ -6,21 +6,22 @@ public static class DateTimeHelper
 {
     public const string DefaultValue = " --- ", DefaultFormat = "d MMM HH:mm", ShortDefaultFormat = "HH:mm";
 
-    public static string ToStringLocalTime(this DateTime? date, int referenceDateDay, [ConstantExpected] string nullValue = DefaultValue, IFormatProvider? formatProvider = null)
+    public static string ToStringLocalTime(this DateTime? date, int referenceLocalDateDay, [ConstantExpected] string nullValue = DefaultValue, IFormatProvider? formatProvider = null)
     {
         if (date is null)
             return nullValue;
 
         DateTime localDate = date.Value.ToLocalTime();
-        if (localDate.Day == referenceDateDay)
-            return ToStringLocalTime(localDate, nullValue, ShortDefaultFormat, formatProvider);
+
+        if (localDate.Day == referenceLocalDateDay)
+            return localDate.ToString(ShortDefaultFormat, formatProvider);
         else
-            return ToStringLocalTime(localDate, nullValue, DefaultFormat, formatProvider);
+            return localDate.ToString(DefaultFormat, formatProvider);
     }
 
-    public static string ToStringLocalTime(this DateTime? date, [ConstantExpected] string nullValue = DefaultValue, [ConstantExpected] string format = DefaultFormat, IFormatProvider? formatProvider = null) =>
+    public static string ToStringLocalTime(this DateTime? date, [ConstantExpected] string nullValue = DefaultValue, [ConstantExpected][StringSyntax(StringSyntaxAttribute.DateTimeFormat)] string format = DefaultFormat, IFormatProvider? formatProvider = null) =>
         date?.ToLocalTime().ToString(format, formatProvider) ?? nullValue;
 
-    public static string ToStringUtcTime(this DateTime? date, [ConstantExpected] string nullValue = DefaultValue, [ConstantExpected] string format = DefaultFormat, IFormatProvider? formatProvider = null) =>
+    public static string ToStringUtcTime(this DateTime? date, [ConstantExpected] string nullValue = DefaultValue, [ConstantExpected][StringSyntax(StringSyntaxAttribute.DateTimeFormat)] string format = DefaultFormat, IFormatProvider? formatProvider = null) =>
         date?.ToUniversalTime().ToString(format, formatProvider) ?? nullValue;
 }
