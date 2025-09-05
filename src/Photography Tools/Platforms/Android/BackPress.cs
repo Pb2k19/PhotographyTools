5b993a7f38a7d28ca75df014dd966f55f13f9a99
Patryk Bartołd <patryk.bartold@wp.pl>
2025-09-05 15:51:46 +0200

    Double click back exit on android

---
+++ src/Photography Tools/Platforms/Android/BackPress.cs
@@ -0,0 +1,37 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Widget;
+using AndroidX.Activity;
+
+namespace Photography_Tools.Platforms.Android;
+
+internal sealed class BackPress : OnBackPressedCallback
+{
+    private const int delay = 3000;
+    private readonly Activity activity;
+    private long backPressed;
+
+    public BackPress(Activity activity) : base(true)
+    {
+        this.activity = activity;
+    }
+
+    public override void HandleOnBackPressed()
+    {
+        INavigation? navigation = Microsoft.Maui.Controls.Application.Current?.Windows[0]?.Navigation;
+
+        if (navigation is not null && navigation.NavigationStack.Count <= 1 && navigation.ModalStack.Count <= 0)
+        {
+            if (backPressed + delay > DateTimeOffset.UtcNow.ToUnixTimeMilliseconds())
+            {
+                activity.FinishAndRemoveTask();
+                Process.KillProcess(Process.MyPid());
+            }
+            else
+            {
+                Toast.MakeText(activity, "Press back again to exit", ToastLength.Short)?.Show();
+                backPressed = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+            }
+        }
+    }
+}
